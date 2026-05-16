c47c28c6c2f629be969d2399e305af2bb2ba4b9a
ayaidrissi06 <aya18idrissi@gmail.com>
2026-05-16 14:08:54 +0100

    vesion login+sign up

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const nameInput = document.getElementById("name");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
-const button = document.getElementById("signup");
+const signupButton = document.getElementById("signup");
 
-button.addEventListener("click", () => {
+const loginEmail = document.getElementById("login-email");
+const loginPassword = document.getElementById("login-password");
+const loginButton = document.getElementById("login");
+
+signupButton.addEventListener("click", () => {
     const nameValue = nameInput.value.trim();
     const emailValue = email.value.trim();
     const passwordValue = password.value;
@@ -21,4 +25,22 @@ button.addEventListener("click", () => {
     nameInput.value = "";
     email.value = "";
     password.value = "";
+});
+
+loginButton.addEventListener("click", () => {
+    const emailValue = loginEmail.value.trim();
+    const passwordValue = loginPassword.value;
+
+    if (!emailValue || !passwordValue) {
+        return alert("Veuillez remplir tous les champs");
+    }
+
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+    const existingUser = users.find(user => user.email === emailValue && user.password === passwordValue);
+
+    if (existingUser) {
+        alert("Connexion réussie");
+    } else {
+        alert("Veuillez vous inscrire");
+    }
 });
